4e6a62eb190004d8200bcdce286e17d61740dd7a
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-08-24 15:00:38 +0530

    def isnonprime(k):     for j in range(2,int(k**0.5)+1):         if k%j==0:             return True     else:         return False n=int(input()) c=0 for i in range(2,n+1):     if n%i==0:         if isnonprime(i):             c=c+1 print(c+1)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,16 +1,13 @@
-def prime(k):
-    for j in range(2,i//2+1):
-        if i%j==0:
-            return False
-            break
+def isnonprime(k):
+    for j in range(2,int(k**0.5)+1):
+        if k%j==0:
+            return True
     else:
-        return True
+        return False
 n=int(input())
-k=0
-c=1
+c=0
 for i in range(2,n+1):
     if n%i==0:
-        c=c+1
-        if prime(i):
-            k=k+1
-print(c-k)
+        if isnonprime(i):
+            c=c+1
+print(c+1)
