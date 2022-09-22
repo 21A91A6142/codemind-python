754002a1c36312192dc18578875422be81f93602
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-09-22 16:32:55 +0530

    a=input() a=a.split() for i in a:     print(i[::-1],end=" ")

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,10 +1,4 @@
-a=str(input())
-b=a.split()
-c=[]
-d=[]
-for i in range(len(b)):
-    c.append(b[i])
-for j in range(len(c)):
-    m=c[j][::-1]
-    d.append(m)
-print(*d)
+a=input()
+a=a.split()
+for i in a:
+    print(i[::-1],end=" ")
