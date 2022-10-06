3056a1c837677bf8d02ce0914b1eed4afb38a696
21a91a6142 <97424411+21a91a6142@users.noreply.github.com>
2022-10-06 10:27:25 +0530

    n=int(input()) a=list(map(int,input().split())) m=max(a) m=str(m) c=0 for i in a:     i=str(i)     if len(m)==len(i):         c=c+1 print(c)

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,22 +1,10 @@
-def isdigit(k):
-    q1=k
-    e=0
-    while(q1):
-        r1=q1%10
-        e=e+1
-        q1=q1//10
-    return e
 n=int(input())
 a=list(map(int,input().split()))
-d=max(a)
-q=d
+m=max(a)
+m=str(m)
 c=0
-while(q):
-    r=q%10
-    c=c+1
-    q=q//10
-b=0
-for i in range(0,n):
-    if isdigit(a[i])==c:
-        b=b+1
-print(b)
+for i in a:
+    i=str(i)
+    if len(m)==len(i):
+        c=c+1
+print(c)
